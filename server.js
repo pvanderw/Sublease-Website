e022be1975d9d53cac2f402655f03b9db6d07d77
pvanderw <pvanderw@usc.edu>
2015-08-20 17:18:58 -0700

    updated server.js

--- server.js
+++ server.js
@@ -10,7 +10,14 @@ app.use(bodyParser.json());
 require('./config/mongoose.js');
 require('./config/routes.js')(app);
 
-app.listen(6789, function()
-{
-	console.log('Listening on port 6789');
+// app.listen(6789, function()
+// {
+// 	console.log('Listening on port 6789');
+// });
+
+
+app.set('port', process.env.PORT || 8000);
+var server = app.listen(app.get('port'), function(){
+	console.log("Express server listening on port " + app.get('port'));
 });
+
